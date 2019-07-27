f532010c32e08c023051d6617321ee54eb3ff606
Ananda9470 <anandan9470@gmail.com>
2019-07-27 19:28:00 +0900

    Fourier SVG working

---
+++ run_fourier.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PYTHONPATH=${PYTHONPATH}:${PWD}
+python ./manim.py active_projects/diffyq/part4/long_fourier_scenes.py FourierOfFourier -pl
--- run_pendulum.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export PYTHONPATH=${PYTHONPATH}:${PWD}
-python ./manim.py active_projects/diffyq/part1/pendulum.py SimpleDampenedPendulum -pl
